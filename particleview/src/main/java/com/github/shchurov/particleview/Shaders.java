c70ab36eac5e37194fd824ad1b95935813e415da
Mykhailo Shchurov <shchurov.m@gmail.com>
2016-11-30 22:45:06 +0200

    added shader precisions

--- particleview/src/main/java/com/github/shchurov/particleview/Shaders.java
+++ particleview/src/main/java/com/github/shchurov/particleview/Shaders.java
@@ -5,10 +5,10 @@ class Shaders {
     static final String V_SHADER = "" +
             "uniform mat4 uMvpMatrix;" +
             "attribute vec4 aPosition;" +
-            "attribute float aAlpha;" +
-            "varying float vAlpha;" +
-            "attribute vec2 aTextureCoords;" +
-            "varying vec2 vTextureCoords;" +
+            "attribute lowp float aAlpha;" +
+            "varying lowp float vAlpha;" +
+            "attribute mediump vec2 aTextureCoords;" +
+            "varying mediump vec2 vTextureCoords;" +
             "void main() {" +
             "  gl_Position = uMvpMatrix * aPosition;" +
             "  vAlpha = aAlpha;" +
@@ -16,9 +16,9 @@ class Shaders {
             "}";
 
     static final String F_SHADER = "" +
-            "varying float vAlpha;" +
-            "uniform sampler2D uTexture;" +
-            "varying vec2 vTextureCoords;" +
+            "varying lowp float vAlpha;" +
+            "uniform mediump sampler2D uTexture;" +
+            "varying mediump vec2 vTextureCoords;" +
             "void main() {" +
             "  gl_FragColor = texture2D(uTexture, vTextureCoords) * vAlpha;" +
             "}";
